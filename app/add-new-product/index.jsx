ba4bf08c0ef90548bcf146e3cfc1fdd64c589c0c
n0vimi00 <milla.viitapohja@gmail.com>
2024-12-05 14:36:50 +0200

    Save new product details to Firestore

--- app/add-new-product/index.jsx
+++ app/add-new-product/index.jsx
@@ -1,21 +1,25 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, Pressable, ToastAndroid } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useNavigation } from 'expo-router'
 import { TextInput } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Colors from '../../constants/Colors';
 import { Picker } from '@react-native-picker/picker';
+import { setDoc, doc } from 'firebase/firestore';
+import { db } from '../../config/FirebaseConfig';
 
 export default function AddNewProduct() {
     const navigation = useNavigation();
-    const [formData, setFormData] = useState({});
+    const [productName, setProductName] = useState({});
     const [stores, setStores] = useState([{ store: '', price: '' }]);
+    const [image, setImage] = useState('');
 
     useEffect(() => {
         navigation.setOptions({
-            headerTitle: 'Lisaa uusi tuote'
+            headerTitle: 'Lisää uusi tuote'
         })
     }, [])
+
 
     const handleStoreChange = (index, key, value) => {
         setStores(prevStores => {
@@ -29,30 +33,73 @@ export default function AddNewProduct() {
         setStores([...stores, { store: '', price: '' }]);
     };
 
-    const onSubmit = () => {
+    const onSubmit = async () => {
         const submissionData = {
-            ...formData,
-            stores, 
+            ...productName,
+            stores,
+            imageURL: image,
         };
-        console.log(submissionData);
+        if (!productName.name) { 
+            ToastAndroid.show('Lisää tuotteen nimi', ToastAndroid.SHORT);
+            return;
+        }
+        try {
+            await SaveFormData(submissionData);
+            ToastAndroid.show('Tuote tallennettu onnistuneesti', ToastAndroid.SHORT);
+            navigation.goBack()
+        } catch (error) {
+            console.error('Error saving data:', error);
+            ToastAndroid.show('Tallennus epäonnistui', ToastAndroid.SHORT);
+        }
+    };
+
+    const SaveFormData = async (submissionData) => {
+        const docId = Date.now().toString();
+        // Transform stores array into a map
+        const storesMap = submissionData.stores.reduce((map, store) => {
+            if (store.store && store.price) {
+                map[store.store] = parseFloat(store.price); 
+            }
+            return map;
+        }, {});
+        try {
+            await setDoc(doc(db, 'products', docId), {
+                ...submissionData,
+                stores: storesMap,
+            });
+            console.log('Data successfully saved to Firestore');
+        } catch (error) {
+            console.error('Error saving data to Firestore:', error);
+            throw error; 
+        }
     };
 
     return (
         <View style={{ padding: 20 }}>
             <View style={styles.imageContainer}>
-                <Image
-                    source={require('./../../assets/images/placeholder.jpeg')}
-                    style={styles.image}
-                />
-                <View style={styles.textContainer}>
-                    <Text style={styles.placeholderText}>Lisaa kuva</Text>
-                </View>
+                {image ? (
+                    <Image source={{ uri: image }} style={styles.image} />
+                ) : (
+                    <Image
+                        source={require('./../../assets/images/placeholder.jpeg')}
+                        style={styles.image}
+                    />
+                )}
             </View>
+     <View style={{  alignSelf: 'center', marginTop:-25, marginBottom:20 }}>
+                {/* <Text style={styles.label}>Kuvan URL</Text> */}
+                <TextInput
+                     style={[styles.input, { width: '70%' }]}
+                    placeholder="Syötä kuvan URL"
+                    value={image}
+                    onChangeText={(value) => setImage(value)}
+                />
+           </View>
             <View style={styles.inputContainer}>
                 <Text style={styles.label}>Tuotteen nimi *</Text>
                 <TextInput
                     style={styles.input}
-                    onChangeText={(value) => setFormData({ ...formData, name: value })}
+                    onChangeText={(value) => setProductName({ ...productName, name: value })}
                 />
             </View>
             {stores.map((storeItem, index) => (
@@ -65,10 +112,10 @@ export default function AddNewProduct() {
                             style={styles.picker}
                         >
                             <Picker.Item label="Valitse kauppa" value="" />
-                            <Picker.Item label="Prisma" value="Prisma" />
-                            <Picker.Item label="K-city" value="K-city" />
-                            <Picker.Item label="Lidl" value="Lidl" />
-                            <Picker.Item label="Tokmanni" value="Tokmanni" />
+                            <Picker.Item label="Prisma" value="prisma" />
+                            <Picker.Item label="K-city" value="k-city" />
+                            <Picker.Item label="Lidl" value="lidl" />
+                            <Picker.Item label="Tokmanni" value="tokmanni" />
                         </Picker>
                     </View>
                     <View style={styles.priceContainer}>
@@ -145,7 +192,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        marginTop:20
+        marginTop: 20
     },
     storeContainer: {
         flex: 2,
