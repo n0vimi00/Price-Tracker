b2636786e1ea923afd0b8399b11e62802782aafa
n0vimi00 <milla.viitapohja@gmail.com>
2024-12-07 21:43:27 +0200

    Add and modify header buttons

--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -3,28 +3,27 @@ import React from 'react';
 import color from "./../../constants/Colors";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { TouchableOpacity } from "react-native";
 import { Link } from "expo-router";
 
 export default function Header() {
   return (
     <View style={{
-      display:"flex",
-      flexDirection:'row',
-      justifyContent:'space-between',
-      marginTop:10,
-      padding:10
+      display: "flex",
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: 10,
+      padding: 10
     }}>
-{/* <Ionicons name="filter" size={40} color={color.secondary} /> */}
+      <Ionicons name="filter-circle-sharp" size={60} color={color.primary} />
       <View>
-      <Text style={{
-        color:color.title,
-        fontFamily:'outfit-medium',
-        fontSize:40,
-      }}>Welcome</Text>
+        <Text style={{
+          color: color.title,
+          fontFamily: 'outfit-medium',
+          fontSize: 40,
+        }}>Hintavahti</Text>
       </View>
       <Link href={'/add-new-product'}>
-      <AntDesign name="pluscircle" size={43} color={color.primary}/>
+        <AntDesign name="pluscircle" size={60} color={color.primary} />
       </Link>
     </View>
   )
